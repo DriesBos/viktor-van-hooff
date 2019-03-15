78a4544ff00d45e82277f70690ca9c0e12984b2e
Dries Bos <info@driesbos.com>
2019-03-15 22:22:00 +0100

    about page to new

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,8 +1,20 @@
 <template>
-  <section class="view-Container view-Profile">
-    <h1>{{ title }}</h1>
-    <p>{{ content }}</p>
-  </section>
+  <div class="profilesItem">
+    <ul>
+      <li>
+        <!-- <img :src="profile.cover.url"> -->
+        <div class="product-TitleDate">
+          <div>
+            <h1>{{ title }}</h1>
+          </div>
+          <div>
+            <!-- <p v-if="profile.categories">{{ profile.categories.title }}</p> -->
+          </div>
+        </div>
+        <p>{{ content }}</p>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
