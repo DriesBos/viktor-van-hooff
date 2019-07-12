a84d3a9b22a7066d8b4c115e7fbccdc9b5f9ae54
Dries Bos <info@driesbos.com>
2019-07-12 17:24:14 +0200

    slideshow component to about page

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -2,9 +2,14 @@
   <div class="view-General view-About">
     <ul class="post-List">
       <li class="post-Item">
-        <div v-if="thumbnail" class="post-Thumbnail" v-lazy-container="{ selector: 'img' }">
+        <a
+          v-if="thumbnail"
+          class="post-Thumbnail"
+          @click="sliderIndex = 0"
+          v-lazy-container="{ selector: 'img' }"
+        >
           <img :data-src="thumbnail | resize('760x0')" width="760" height="auto" />
-        </div>
+        </a>
         <div class="post-Info">
           <div v-if="title">
             <h1>{{ title }}</h1>
@@ -13,15 +18,27 @@
         <MarkdownItem v-if="content" :input="content" class="post-Content" />
       </li>
     </ul>
+    <modalItem v-if="sliderIndex > -1" @close="sliderIndex = -1">
+      <sliderItem @close="sliderIndex = -1" :index="sliderIndex" :images="[thumbnail]"></sliderItem>
+    </modalItem>
   </div>
 </template>
 
 <script>
 import MarkdownItem from '~/components/MarkdownItem.vue'
+import SliderItem from '~/components/SliderItem.vue'
+import ModalItem from '~/components/ModalItem.vue'
 
 export default {
   components: {
-    MarkdownItem
+    MarkdownItem,
+    SliderItem,
+    ModalItem
+  },
+  data() {
+    return {
+      sliderIndex: -1
+    }
   },
   asyncData(context) {
     return context.app.$storyapi
